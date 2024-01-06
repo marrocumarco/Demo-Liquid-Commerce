c955c5f9d1db8865ce7b27cfadb90afaa6fe40d5
Marco Marrocu <marrocumarcozaggi@gmail.com>
2024-01-06 14:40:42 +0100

    StoreClient injected to the main view model

--- Demo Liquid Commerce/ViewModels/MainViewViewModel.swift
+++ Demo Liquid Commerce/ViewModels/MainViewViewModel.swift
@@ -7,7 +7,20 @@
 
 import Foundation
 
+enum AuthenticationType
+{
+    case base
+    case oAuth
+}
+
 class MainViewViewModel: ObservableObject
 {
+    let client: StoreClient
+    let productListViewModel: ProductsListViewModel
     
+    internal init(client: StoreClient) 
+    {
+        self.client = client
+        productListViewModel = ProductsListViewModel(client: client)
+    }
 }
--- Demo Liquid Commerce/Views/MainView.swift
+++ Demo Liquid Commerce/Views/MainView.swift
@@ -13,7 +13,7 @@ struct MainView: View {
 
     var body: some View {
             TabView {
-                ProductsListView(productsListViewModel: ProductsListViewModel(client: try! OAuthClient(basePath: StringConstants.testBasePath.rawValue)))
+                ProductsListView(productsListViewModel: ProductsListViewModel(client: viewModel.client))
                     .tabItem {
                         Label(LocalizedStringKey("Menu"), systemImage: "wineglass")
                     }
@@ -27,5 +27,5 @@ struct MainView: View {
 }
 
 #Preview {
-    MainView(viewModel: MainViewViewModel())
+    MainView(viewModel: MainViewViewModel(client: try! OAuthClient(basePath: StringConstants.testBasePath.rawValue)))
 }
