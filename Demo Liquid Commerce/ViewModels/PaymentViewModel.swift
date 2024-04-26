30220c44cf9d3ca4c263d85afd7f6cb15a73bc0e
Marco Marrocu <marrocumarcozaggi@gmail.com>
2024-04-26 17:52:35 +0200

    added checkout view and payment view model

---
+++ Demo Liquid Commerce/ViewModels/PaymentViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  PaymentViewModel.swift
+//  Demo Liquid Commerce
+//
+//  Created by marrocumarco on 26/04/2024.
+//
+import StripePaymentSheet
+import SwiftUI
+
+public class PaymentViewModel: ObservableObject
+{
+    
+    @Published var paymentSheet: PaymentSheet?
+    @Published var paymentResult: PaymentSheetResult?
+    var products = [Product]()
+    let paymentClient: PaymentClient
+    
+    init(paymentClient: PaymentClient)
+    {
+        self.paymentClient = paymentClient
+    }
+    
+    func preparePaymentSheet() async throws 
+    {
+        // MARK: Fetch the PaymentIntent and Customer information from the backend
+        let response = try await paymentClient.startCheckout(products)
+        
+        STPAPIClient.shared.publishableKey = response.publishableKey
+        // MARK: Create a PaymentSheet instance
+        var configuration = PaymentSheet.Configuration()
+        configuration.merchantDisplayName = "Example, Inc."
+        configuration.customer = .init(id: response.customer, ephemeralKeySecret: response.ephemeralKey)
+        // Set `allowsDelayedPaymentMethods` to true if your business can handle payment methods
+        // that complete payment after a delay, like SEPA Debit and Sofort.
+        configuration.allowsDelayedPaymentMethods = false
+        
+        self.paymentSheet = PaymentSheet(paymentIntentClientSecret: response.paymentIntent ?? "", configuration: configuration)
+        
+    }
+    
+    func onPaymentCompletion(result: PaymentSheetResult) 
+    {
+        self.paymentResult = result
+    }
+}
---
+++ Demo Liquid Commerce/Views/CheckoutView.swift
@@ -0,0 +1,42 @@
+//
+//  CheckoutView.swift
+//  Demo Liquid Commerce
+//
+//  Created by marrocumarco on 26/04/2024.
+//
+
+import SwiftUI
+import StripePaymentSheet
+
+struct CheckoutView: View {
+    @ObservedObject var model = PaymentViewModel(paymentClient: StripeClient())
+
+  var body: some View {
+    VStack {
+      if let paymentSheet = model.paymentSheet {
+        PaymentSheet.PaymentButton(
+          paymentSheet: paymentSheet,
+          onCompletion: model.onPaymentCompletion
+        ) {
+          Text("Buy")
+        }
+      } else {
+        Text("Loading…")
+      }
+      if let result = model.paymentResult {
+        switch result {
+        case .completed:
+          Text("Payment complete")
+        case .failed(let error):
+            Text("Payment failed: \(error.localizedDescription)")
+        case .canceled:
+            Text("Payment canceled.")
+        }
+      }
+    }.onAppear {
+        Task{
+            try await model.preparePaymentSheet()
+        }
+    }
+  }
+}
